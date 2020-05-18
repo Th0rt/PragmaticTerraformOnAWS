5ad4692ab28c4e03cb238e9adb87df4b6b07cd93
Tsukasa Yamada <yamada.tsukasa@panair.jp>
2020-05-18 20:45:00 +0900

    fix typo

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ variable "aws_secret_access_key" {}
 
 provider "aws" {
   access_key = var.aws_access_key
-  secret_key = var.aws_secreate_access_key
+  secret_key = var.aws_secret_access_key
   region     = "us-east-1"
 }
 
